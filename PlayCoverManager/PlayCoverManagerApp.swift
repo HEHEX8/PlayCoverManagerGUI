1bbaed9a10b3a128879676c49c4c76a9bc2496e7
HEHEX8 <hex6ji@gmail.com>
2025-11-05 15:09:20 +0900

    Initial Commit

---
+++ PlayCoverManager/PlayCoverManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayCoverManagerApp.swift
+//  PlayCoverManager
+//
+//  Created by HEHEX on 2025/11/05.
+//
+
+import SwiftUI
+
+@main
+struct PlayCoverManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
